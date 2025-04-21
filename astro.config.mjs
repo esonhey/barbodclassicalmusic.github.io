04808d97a8bc8a098a11612bce667c66b0c4489f
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2025-04-21 15:16:24 +0200

    migrate to astro

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://barbodclassicalmusic.com",
+
+  integrations: [mdx(), sitemap()],
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
+});
