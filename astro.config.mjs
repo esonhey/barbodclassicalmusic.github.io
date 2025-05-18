e1113a766285c68a7a536415b7e9ae184d457075
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2025-05-18 21:32:39 +0200

    integrate with cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://barbodclassicalmusic.com",
   integrations: [react(), mdx(), sitemap()],
-  output: "static", 
-  outDir: './dist',
-  publicDir: './public',
+  adapter: cloudflare({
+    platformProxy: {
+      enabled: true,
+    },
+  }),
 });
-
