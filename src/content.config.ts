a9223754439e530179bcaedfba0281cef97c8f3b
Ehsan Heydari <ehsan.heydari.yk@gmail.com>
2025-05-18 22:29:56 +0200

    refac

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,7 +10,6 @@ const blog = defineCollection({
     description: z.string(),
     // Transform string to Date object
     pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
     heroImage: z.string().optional(),
   }),
 });
